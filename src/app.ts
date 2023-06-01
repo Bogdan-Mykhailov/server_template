3f6f85f4ae057b81c0b1eac35b62f27f37c39e1e
Bogdan <bogdan_mykhailov@icloud.com>
2023-06-01 12:17:45 +0300

    :tada: Initial commit

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+window.console.log('heelo');
+
+function check(value) {
+  console.log('sdfsdfkjlkwjeflkj');
+
+  if (value === null) {
+    console.log('dsdfsdfsdf');
+  }
+
+  const t = typeof value;
+}
+
+check('ssd');
