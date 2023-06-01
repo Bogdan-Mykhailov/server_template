8a7a2ec1300f63ad8c797f09255a8f50a35d4dfb
Bogdan <bogdan_mykhailov@icloud.com>
2023-06-01 13:53:07 +0300

    :construction: add husky

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,1 @@
-window.console.log('heelo');
-
-function check(value) {
-  console.log('sdfsdfkjlkwjeflkj');
-
-  if (value === null) {
-    console.log('dsdfsdfsdf');
-  }
-
-  const t = typeof value;
-}
-
-check('ssd');
+'use strict';
