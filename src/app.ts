1aa71306fcf211034cbc84aa504b509e1f1ed52d
Bogdan <bogdan_mykhailov@icloud.com>
2023-07-28 16:16:44 +0300

    :construction: add express and create server

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,18 @@
 'use strict';
+
+import express from 'express';
+
+const PORT = 4000;
+const createServer = () => {
+  const app = express();
+
+  app.use((req, res) => {
+    res.send('Hello World');
+  });
+
+  return app;
+};
+
+createServer().listen(PORT, () => {
+  window.console.log(`Server is running on port ${PORT}`);
+});
